7c2495321218e689373a643cba6a2d06e5831455
tronxi <sergio.gsanchez97@gmail.com>
2021-11-05 20:07:42 +0100

    ejercicio 1

---
+++ practica3/ejercicio1/run.sh
@@ -0,0 +1,1 @@
+python3 space.py space.json --cleanup=NONE
---
+++ practica3/ejercicio1/space.py
@@ -0,0 +1,34 @@
+from mrjob.job import MRJob
+import json
+import re
+from collections import Counter
+
+class Space(MRJob):
+
+
+    def mapper(self, key, line):
+        lineJson = json.loads(line)
+        content = self.clean(lineJson["content"])
+        palabras = content.split()
+        conteo = Counter(palabras)
+        for p in palabras:
+            yield(p, (conteo[p], lineJson["filename"]))
+    
+    def clean(self, line):
+        # return re.sub("(\||\.|@|,|\"|\'|>|<|\-|\)|\(|\:|!|\?|\=\+)|\/", "", line).lower()
+        return re.sub("[^\w\s]", "", line).lower()
+
+
+    def combiner(self, key, values):
+        ls = sorted(list(values), reverse=True)[:1]
+        for item in ls:
+            yield (key, item)
+
+    def reducer(self, key, values):
+        ls = sorted(list(values), reverse=True)[:1]
+        for item in ls:
+            yield (key, (item[1], item[0]))
+
+
+if __name__ == '__main__':
+    Space.run()
