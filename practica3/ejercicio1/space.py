a990a937ad74cfbcf041a35f1499585193235f1c
tronxi <sergio.gsanchez97@gmail.com>
2021-11-20 15:42:40 +0100

    cabecer

--- practica3/ejercicio1/space.py
+++ practica3/ejercicio1/space.py
@@ -1,3 +1,11 @@
+# SGDI, Práctica MapReduce - Apache Spark, Sergio García Sánchez, Miguel 
+# Declaramos que esta solución es fruto exclusivamente de nuestro tra-
+# bajo personal. No hemos sido ayudados por ninguna otra persona ni
+# hemos obtenido la solución de fuentes externas, y tampoco hemos
+# compartido nuestra solución con otras personas. Declaramos además
+# que no hemos realizado de manera deshonesta ninguna otra actividad
+# que pueda mejorar nuestros resultados ni perjudicar los resultados de
+# los demás.
 from mrjob.job import MRJob
 import json
 import re
